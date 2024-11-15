b40b5e0656e2f9cc99454d3277b09a89803ab68b
Akmal <jasminaaa.20@uom.lk>
2024-11-15 23:11:35 +0530

    Improved Code quality

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a new "Add to Group" button by cloning and modifying the Like button.
+ * @param {HTMLElement} likeButton - The existing Like button element to clone.
+ * @returns {HTMLElement} The new "Add to Group" button wrapped in a div.
+ */
 function createDynamicAddToGroupButton(likeButton) {
 
   const addToGroupButton = likeButton.cloneNode(true);
@@ -9,42 +14,63 @@ function createDynamicAddToGroupButton(likeButton) {
   const svg = addToGroupButton.querySelector("svg");
   if (svg) {
     svg.innerHTML = "";
-    const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path.setAttribute("d", "M12 3.5v17M3.5 12h17");
-    path.setAttribute("fill", "none");
-    path.setAttribute("stroke", "currentColor");
-    path.setAttribute("stroke-width", "2");
-
-    g.appendChild(path);
-    svg.appendChild(g);
+    svg.appendChild(createPlusIconG());
   }
 
-  const textSpan = addToGroupButton.querySelector("span[data-testid='app-text-transition-container'] span span");
+  const textSpan = addToGroupButton.querySelector("span span span");
   if (textSpan) {
     textSpan.textContent = "Add";
   }
 
-  const innerDiv = addToGroupButton.querySelector('div[dir="ltr"]');
-  if (innerDiv) {
-    innerDiv.style.color = "rgb(113, 118, 123)";
+  const contentDiv = addToGroupButton.querySelector('div[dir="ltr"]');
+  if (contentDiv) {
+    contentDiv.style.color = "rgb(113, 118, 123)";
   }
 
   addToGroupButton.addEventListener("click", openGroupPopup);
 
   const buttonDiv = document.createElement("div");
-  buttonDiv.classList.add("css-175oi2r", "r-18u37iz", "r-1h0z5md", "r-13awgt0");
-
+  buttonDiv.classList.add(
+    "css-175oi2r", 
+    "r-18u37iz", 
+    "r-1h0z5md", 
+    "r-13awgt0"
+  );
   buttonDiv.appendChild(addToGroupButton);
 
   return buttonDiv;
+}
+
+/**
+ * Creates an SVG <g> element containing a single <path> element with a plus (+)
+ * symbol. The element is intended to be used as the icon for the "Add to Group"
+ * button. The element is created with the colors and styling of the Like button
+ * icon.
+ * @returns {SVGGElement} The created SVG <g> element.
+ */
+function createPlusIconG() {
+  const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", "M12 3.5v17M3.5 12h17");
+  path.setAttribute("fill", "none");
+  path.setAttribute("stroke", "currentColor");
+  path.setAttribute("stroke-width", "2");
+
+  g.appendChild(path);
+
+  return g;
 }
 
 function openGroupPopup() {
   alert("Add to Group popup would appear here.");
 }
 
+/**
+ * Injects the "Add to Group" button into every tweet that doesn't already have
+ * it. The button is inserted after the "Like" button.
+ * @returns {void}
+ */
 function injectAddToGroupButton() {
   document.querySelectorAll('article[data-testid="tweet"]').forEach(tweet => {
     const likeButton = tweet.querySelector('button[data-testid="like"], button[data-testid="unlike"]');
@@ -56,7 +82,18 @@ function injectAddToGroupButton() {
   });
 }
 
-injectAddToGroupButton();
+/**
+ * Injects the "Add to Group" button into every tweet that doesn't already have
+ * it, and then starts observing the DOM for any changes to the tweets. When the
+ * DOM changes, the function is called again, which will re-inject the buttons
+ * into any new tweets that have been added.
+ * @returns {void}
+ */
+function observeAndInjectButtons() {
+  injectAddToGroupButton();
 
-const observer = new MutationObserver(injectAddToGroupButton);
-observer.observe(document.body, { childList: true, subtree: true });
+  const observer = new MutationObserver(injectAddToGroupButton);
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+observeAndInjectButtons();
