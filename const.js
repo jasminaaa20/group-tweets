36500c73322f4c57e21b9811889a63b570e8418f
Akmal <jasminaaa.20@uom.lk>
2024-11-16 20:02:55 +0530

    Add CSS class and color constants to const.js

---
+++ const.js
@@ -0,0 +1,18 @@
+// CSS class constants
+export const CSS_CLASSES = {
+    buttonWrapper: ["css-175oi2r", "r-18u37iz", "r-1h0z5md", "r-13awgt0"],
+    textSpan: [
+      "css-1jxf684",
+      "r-bcqeeo",
+      "r-1ttztb7",
+      "r-qvutc0",
+      "r-poiln3"
+    ],
+  };
+  
+  // Color constants
+  export const COLORS = {
+    neutral: "rgb(113, 118, 123)", // Gray color
+    active: "rgb(29, 155, 240)",   // Blue color
+  };
+  
